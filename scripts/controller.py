02a4607b72f9339d855aef04e702c958bc261bd5
tact1m4n3 <tudor@dicu.org>
2026-08-17 13:25:41 +0300

    Implement object detection

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+import threading
 import rospy
 import math
 import actionlib
+import cv_bridge
+import numpy as np
+import tf
+import tf.transformations as tft
+
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from tiago_project.msg import ControllerSpinAction, ControllerSpinFeedback
 from tiago_project.msg import ControllerNavigateAction, ControllerNavigateFeedback
 from tf.transformations import euler_from_quaternion
@@ -31,6 +37,7 @@ def normalize_angle(angle):
 
 class Controller:
     def __init__(self):
+        self.state_lock = threading.Lock()
         self.busy = False
 
         self.cmd_pub = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=10)
@@ -51,106 +58,232 @@ class Controller:
         )
         self.navigate_server.start()
         self.odom_tracker = OdomTracker()
+
         self.camera_topic = '/xtion/rgb/image_raw'
+        self.depth_topic = '/xtion/depth_registered/image_raw'
+        self.camera_info_topic = '/xtion/rgb/camera_info'
+
+        rospy.loginfo("Fetching camera intrinsics from {}...".format(self.camera_info_topic))
+        try:
+            info_msg = rospy.wait_for_message(self.camera_info_topic, CameraInfo, timeout=5.0)
+
+            # Extract values from the flattened 9-element K matrix
+            self.camera_info = {
+                "fx": info_msg.K[0],
+                "cx": info_msg.K[2],
+                "fy": info_msg.K[4],
+                "cy": info_msg.K[5],
+            }
+
+            rospy.loginfo("Camera intrinsics locked: fx={:.1f}, fy={:.1f}, cx={:.1f}, cy={:.1f}".format(
+                self.camera_info["fx"], self.camera_info["fy"], self.camera_info["cx"], self.camera_info["cy"]))
+
+        except rospy.ROSException:
+            rospy.logerr("Failed to get camera_info! Falling back to TIAGo defaults.")
+
+            self.camera_info = {
+                "fx": 525.0,
+                "cx": 319.5,
+                "fy": 525.0,
+                "cy": 239.5,
+            }
+
+        self.snapshot_memory = {}
+        self.bridge = cv_bridge.CvBridge()
+        self.tf_listener = tf.TransformListener()
 
         rospy.loginfo("Ready?! Vodafone!")
 
     def spin_callback(self, goal):
-        if self.busy:
-            rospy.logwarn("Me busy!!! Stop bothering!")
-            self.spin_server.set_aborted()
-            return
+        with self.state_lock:
+            if self.busy:
+                rospy.logwarn("Me busy!!! Stop bothering!")
+                self.spin_server.set_aborted()
+                return
+            self.busy = True
 
-        self.busy = True
+        try:
+            rospy.loginfo("Starting work: Take {} pictures every {}.".format(goal.num_pictures, goal.step_angle))
 
-        # Python 2 compatibility: replaced f-string with .format()
-        rospy.loginfo("Starting work: Take {} pictures every {}.".format(goal.num_pictures, goal.step_angle))
+            step_angle_rad = math.radians(goal.step_angle)
+            max_angular_speed = 1.0
+            rotation_time = step_angle_rad / max_angular_speed
+            initial_yaw = self.odom_tracker.current_yaw
+            rate = rospy.Rate(100)
 
-        step_angle_rad = math.radians(goal.step_angle)
-        max_angular_speed = 0.5
-        rotation_time = step_angle_rad / max_angular_speed
-        initial_yaw = self.odom_tracker.current_yaw
-        rate = rospy.Rate(100)
+            feedback = ControllerSpinFeedback()
 
-        feedback = ControllerSpinFeedback()
-
-        for i in range(goal.num_pictures):
-            if self.spin_server.is_preempt_requested():
-                self.spin_server.set_preempted()
-                rospy.loginfo("Spin preempted!")
-                break
-
-            # Pause for 1 second to let the camera physically stabilize before the next shot
-            rospy.sleep(1.0)
-
-            c_angle_rad = self.odom_tracker.current_yaw
-            c_angle_deg = math.degrees(c_angle_rad)
-            i_angle_rad = normalize_angle(initial_yaw + (i * step_angle_rad))
-            i_angle_deg = math.degrees(i_angle_rad)
-            error_rad = normalize_angle(c_angle_rad - i_angle_rad)
-            error_deg = math.degrees(error_rad)
-                    
-            rospy.loginfo("Picture {} | Target: {:.2f} deg | Actual: {:.2f} deg | Error: {:.2f} deg".format(
-                i+1, i_angle_deg, c_angle_deg, error_deg
-            ))
-           
-            try:
-                image = rospy.wait_for_message(self.camera_topic, Image, timeout=5.0)
-
-                feedback.image_id = i
-                feedback.image_data = image
-                self.spin_server.publish_feedback(feedback)
-
-                rospy.loginfo("Feedback id={} published".format(i))
-
-            except rospy.ROSException:
-                rospy.logwarn("Timeout! Failed to get image from {}".format(self.camera_topic))
-
-
-            target_yaw = normalize_angle(initial_yaw + ((i + 1) * step_angle_rad))
-            vel_msg = Twist()
-         
-            while not (rospy.is_shutdown() or self.spin_server.is_preempt_requested()):
-                error = normalize_angle(target_yaw - self.odom_tracker.current_yaw)
-                     
-                if abs(error) < 0.02: 
+            for i in range(goal.num_pictures):
+                if self.spin_server.is_preempt_requested():
+                    self.spin_server.set_preempted()
+                    rospy.loginfo("Spin preempted!")
                     break
-                             
-                p_speed = 0.8 * error
-                         
-                if p_speed > 0:
-                    vel_msg.angular.z = min(max(p_speed, 0.1), max_angular_speed)
-                else:
-                    vel_msg.angular.z = max(min(p_speed, -0.1), -max_angular_speed)
-                        
-                self.cmd_pub.publish(vel_msg)
-                rate.sleep()
-                    
-            self.cmd_pub.publish(Twist())
 
-        rospy.loginfo("Work is done here! Spinned enough! Me no busy!")
+                # Pause for 1 second to let the camera physically stabilize before the next shot
+                rospy.sleep(1.0)
 
-        self.busy = False
-        self.spin_server.set_succeeded()
+                c_angle_rad = self.odom_tracker.current_yaw
+                c_angle_deg = math.degrees(c_angle_rad)
+                i_angle_rad = normalize_angle(initial_yaw + (i * step_angle_rad))
+                i_angle_deg = math.degrees(i_angle_rad)
+                error_rad = normalize_angle(c_angle_rad - i_angle_rad)
+                error_deg = math.degrees(error_rad)
+
+                rospy.loginfo("Picture {} | Target: {:.2f} deg | Actual: {:.2f} deg | Error: {:.2f} deg".format(
+                    i+1, i_angle_deg, c_angle_deg, error_deg
+                ))
+
+                try:
+                    image = rospy.wait_for_message(self.camera_topic, Image, timeout=5.0)
+                    depth_image = rospy.wait_for_message(self.depth_topic, Image, timeout=5.0)
+
+                    # Grab the exact transform from Map to Camera Lens
+                    self.tf_listener.waitForTransform('/map', '/xtion_rgb_optical_frame', rospy.Time(0), rospy.Duration(1.0))
+                    (trans, rot) = self.tf_listener.lookupTransform('/map', '/xtion_rgb_optical_frame', rospy.Time(0))
+
+                    self.snapshot_memory[i] = {
+                        'depth': depth_image,
+                        'trans': trans,
+                        'rot': rot
+                    }
+
+                    # TODO: maybe implement persistent image ids, so that past
+                    # images from previous scans are also available
+                    feedback.image_id = i
+                    feedback.image_data = image
+                    self.spin_server.publish_feedback(feedback)
+
+                    rospy.loginfo("Feedback id={} published".format(i))
+
+                except rospy.ROSException:
+                    rospy.logwarn("Timeout! Failed to get image from {}".format(self.camera_topic))
+                except tf.Exception as e:
+                    rospy.logwarn("TF Error while taking picture: {}".format(e))
+
+                target_yaw = normalize_angle(initial_yaw + ((i + 1) * step_angle_rad))
+                vel_msg = Twist()
+
+                while not (rospy.is_shutdown() or self.spin_server.is_preempt_requested()):
+                    error = normalize_angle(target_yaw - self.odom_tracker.current_yaw)
+
+                    if abs(error) < 0.02:
+                        break
+
+                    p_speed = 0.8 * error
+
+                    if p_speed > 0:
+                        vel_msg.angular.z = min(max(p_speed, 0.1), max_angular_speed)
+                    else:
+                        vel_msg.angular.z = max(min(p_speed, -0.1), -max_angular_speed)
+
+                    self.cmd_pub.publish(vel_msg)
+                    rate.sleep()
+
+                self.cmd_pub.publish(Twist())
+
+            rospy.loginfo("Work is done here! Spinned enough! Me no busy!")
+
+        finally:
+            with self.state_lock:
+                self.busy = False
+            self.spin_server.set_succeeded()
 
     def navigate_callback(self, goal):
-        if self.busy:
-            rospy.logwarn("Me busy!!! Stop bothering!")
-            self.navigate_server.set_aborted()
-            return
+        with self.state_lock:
+            if self.busy:
+                rospy.logwarn("Me busy!!! Stop bothering!")
+                self.navigate_server.set_aborted()
+                return
+            self.busy = True
 
-        self.busy = True
+        try:
+            rospy.loginfo("Deprojecting pixel ({}, {}) from image {}...".format(
+                goal.target_u, goal.target_v, goal.image_id))
 
-        # Python 2 compatibility: replaced f-string with .format()
-        rospy.loginfo("Starting work: Sailing to [{}, {}].".format(goal.x, goal.y))
+            if goal.image_id not in self.snapshot_memory:
+                rospy.logerr("Memory error! I have no data for image {}.".format(goal.image_id))
+                self.navigate_server.set_aborted()
+                return
 
-        # TODO: Implement navigation
-        rospy.sleep(1.0)
+            snapshot = self.snapshot_memory[goal.image_id]
+            trans = snapshot['trans']
+            rot = snapshot['rot']
 
-        rospy.loginfo("Work is done here! Got sea sick! Me no busy!")
+            try:
+                # Convert depth ROS message to OpenCV array
+                depth_array = self.bridge.imgmsg_to_cv2(snapshot['depth'], desired_encoding="passthrough")
+            except cv_bridge.CvBridgeError as e:
+                rospy.logerr("CvBridge Error: {}".format(e))
+                self.navigate_server.set_aborted()
+                return
 
-        # TODO: We should abort if we can't reach the destination
-        self.navigate_server.set_succeeded()
+            # Get the actual dimensions of the saved depth image
+            height, width = depth_array.shape[:2]
+
+            # Convert [0, 1] normalized floats to absolute integer pixels
+            target_u = int(goal.target_u * width)
+            target_v = int(goal.target_v * height)
+
+            # Clamp the values just in case the LLM returned exactly 1.0
+            # (which would cause an out-of-bounds array error)
+            target_u = min(max(target_u, 0), width - 1)
+            target_v = min(max(target_v, 0), height - 1)
+
+            # Get a 5x5 patch around the pixel to avoid NaN holes
+            patch = depth_array[
+                max(0, target_v-2) : min(height, target_v+3),
+                max(0, target_u-2) : min(width, target_u+3)
+            ]
+
+            # Filter valid depths
+            valid_depths = patch[(patch > 0) & (~np.isnan(patch))]
+
+            if len(valid_depths) == 0:
+                rospy.logerr("Depth sensor blind spot at this pixel! Aborting.")
+                self.navigate_server.set_aborted()
+                return
+
+            # Median distance straight out from the camera lens
+            Z = np.median(valid_depths)
+
+            # If depth is in millimeters (16UC1 format), convert to meters
+            if depth_array.dtype == np.uint16:
+                Z = Z / 1000.0
+
+            # Camera Intrinsics
+            fx = self.camera_info["fx"]
+            fy = self.camera_info["fy"]
+            cx = self.camera_info["cx"]
+            cy = self.camera_info["cy"]
+
+            # Pinhole model deprojection
+            X_camera = (target_u - cx) * Z / fx
+            Y_camera = (target_v - cy) * Z / fy
+            Z_camera = Z
+
+            # Apply the frozen transform
+            matrix = tft.quaternion_matrix(rot)
+            matrix[0:3, 3] = trans
+
+            point_camera = np.array([X_camera, Y_camera, Z_camera, 1.0])
+            point_map = np.dot(matrix, point_camera)
+
+            target_x = point_map[0]
+            target_y = point_map[1]
+
+            # ---------------------------------------------------------
+
+            rospy.loginfo("Starting work: Sailing to [{:.2f}, {:.2f}].".format(target_x, target_y))
+
+            # TODO: Implement actual navigation driving here
+            rospy.sleep(1.0)
+
+            rospy.loginfo("Work is done here! Got sea sick! Me no busy!")
+
+        finally:
+            with self.state_lock:
+                self.busy = False
+            self.navigate_server.set_succeeded()
 
 
 if __name__ == '__main__':
@@ -161,5 +294,4 @@ if __name__ == '__main__':
         rospy.sleep(0.1)
 
     controller = Controller()
-
     rospy.spin()
--- scripts/pilot.py
+++ scripts/pilot.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 
-import sys
+import threading
+import queue
 import base64
 import json
 import cv2
 import rospy
 import actionlib
 import cv_bridge
-
-# Python 2 vs Python 3 URL library compatibility
-try:
-    import urllib.request as urllib_req
-    import urllib.error as urllib_err
-except ImportError:
-    import urllib2 as urllib_req
-    import urllib2 as urllib_err
+import urllib2 as urllib_req
 
 from tiago_project.msg import ControllerSpinAction, ControllerSpinGoal
 from tiago_project.msg import ControllerNavigateAction, ControllerNavigateGoal
-from tiago_project.prompts import prompt_instruction_parser
+from tiago_project.prompts import prompt_instruction_parser, prompt_object_detection
 
-API_URL = "http://10.41.2.68:8000/v1/chat/completions"
+API_URL = "http://192.168.1.101:8000/v1/chat/completions"
+
+SPIN_STEP_ANGLE = 45.0
+SPIN_STEPS = 8
+QUEUE_SIZE = 8
+VLM_IMAGE_SIZE = (1280.0, 720.0)
 
 class Pilot:
     def __init__(self):
@@ -38,40 +37,85 @@ class Pilot:
         self.done_spinning = False
         self.targets = []
 
+        self.spin_image_queue = queue.Queue(SPIN_STEPS)
+
     # TODO: can be structured as a service
     def execute_task(self, instructions):
-        # self.task_prompt = parse_instructions_prompt(instructions)
-        # if not self.task_prompt:
-        #     rospy.logerr("Not an interesting command! I have the right to ignore you!")
-        #     return False
-
-        self.done_spinning = False
-        self.targets = []
-
-        # start spinning
-        self.spin_client.send_goal(ControllerSpinGoal(num_pictures=8, step_angle=45.0), feedback_cb=self.spin_feedback_callback)
-        if not self.spin_client.wait_for_result():
-            rospy.logerr("Spin failed! Aborting command!")
+        target = parse_instructions_prompt(instructions)
+        if target is None:
+            rospy.logerr("Not an interesting command! I have the right to ignore you!")
             return False
 
-        while not self.done_spinning:
-            rospy.sleep(1.0)
+        rospy.loginfo("Command is to go to %s (keep %s in mind)!", target["target"], target["desc"])
 
-        # max_score = -1
-        # max_target = None
-        # for target in self.targets:
-        #     if target.score > max_score:
-        #         max_score = target.score
-        #         max_target = target.coord
+        # clear queue (maybe there is a better way)
+        while True:
+            try:
+                self.spin_image_queue.get_nowait()
+            except queue.Empty as e:
+                break
 
-        # if max_target is None:
-        #     rospy.logerr("TODO: No target found!")
-        #     return False
+        # start spinning
+        self.spin_client.send_goal(ControllerSpinGoal(num_pictures=SPIN_STEPS, step_angle=SPIN_STEP_ANGLE), feedback_cb=self.spin_feedback_callback)
 
-        # self.navigate_client.send_goal_and_wait(ControllerNavigateGoal(x = max_target[0], y = max_target[1]))
-        # if not self.spin_client.wait_for_result():
-        #     rospy.logerr("Failed to navigate to destination!")
-        #     return False
+        images_processed = 0
+        found_target = None
+        found_image_id = None
+
+        while images_processed < SPIN_STEPS and not rospy.is_shutdown():
+            try:
+                image_id, image_data = self.spin_image_queue.get(timeout=1.0)
+                images_processed += 1
+
+                rospy.loginfo("Prompting picture %d!", image_id + 1)
+                result = detect_targets_in_image(target, image_data)
+                rospy.loginfo("LLM returned: %s", result)
+
+                if len(result) > 0:
+                    found_image_id, found_target = (image_id, result[0])
+                    rospy.loginfo("Target found! Canceling remaining spin.")
+
+                    current_state = self.spin_client.get_state()
+                    if current_state in [actionlib.GoalStatus.PENDING, actionlib.GoalStatus.ACTIVE]:
+                        rospy.loginfo("Canceling the remaining spin.")
+                        self.spin_client.cancel_goal()
+                    else:
+                        rospy.loginfo("Spin was already finished. No need to cancel.")
+
+                    break
+
+            except queue.Empty:
+                # If we waited 1 second and got no picture, check if the action server stopped
+                state = self.spin_client.get_state()
+
+                # If the server succeeded, aborted, or was preempted, it won't send more images
+                if state in [actionlib.GoalStatus.SUCCEEDED, actionlib.GoalStatus.ABORTED, actionlib.GoalStatus.PREEMPTED]:
+                    rospy.logwarn("Spin action ended early. Stopping image processing.")
+                    break
+
+                # Otherwise, it's just taking a while. Loop around and wait again.
+                continue
+
+        if not self.spin_client.wait_for_result():
+            rospy.logerr("Spinning failed for some obscure reason!")
+            return False
+
+        if found_target is None:
+            rospy.logerr("Target not found!")
+            return False
+
+        true_x_min = found_target["box"]["x_min"] / 1000.0
+        true_y_min = found_target["box"]["y_min"] / 1000.0
+        true_x_max = found_target["box"]["x_max"] / 1000.0
+        true_y_max = found_target["box"]["y_max"] / 1000.0
+
+        # 2. Find the center pixel for the robot to navigate to in [0, 1] range
+        center_u = (true_x_min + true_x_max) / 2
+        center_v = (true_y_min + true_y_max) / 2
+
+        rospy.loginfo("Object center is at true pixel: u={}, v={}".format(center_u, center_v))
+
+        self.navigate_client.send_goal_and_wait(ControllerNavigateGoal(target_u = center_u, target_v = center_v, image_id=found_image_id))
 
         return True
 
@@ -79,8 +123,7 @@ class Pilot:
         rospy.loginfo("Pilot received picture %d!", feedback.image_id + 1)
         rospy.loginfo("Image data size: %d bytes.", len(feedback.image_data.data))
 
-        result = prompt_model("Write a sentence that best describes what is in this image", feedback.image_data)
-        rospy.loginfo("LLM returned: %s", result)
+        self.spin_image_queue.put((feedback.image_id, feedback.image_data))
 
 bridge = cv_bridge.CvBridge()
 
@@ -93,7 +136,7 @@ def encode_image(img):
         return None
 
     h, w = cv_img.shape[:2]
-    max_w, max_h = 1280.0, 720.0
+    max_w, max_h = VLM_IMAGE_SIZE
 
     scale = min(max_w / w, max_h / h)
 
@@ -115,7 +158,7 @@ def encode_image(img):
         raw_bytes = buffer.tostring()
 
     encoded = base64.b64encode(raw_bytes)
-    
+
     # Ensure it's returned as a unicode string on both Python 2 and 3
     if isinstance(encoded, bytes):
         return encoded.decode("utf-8")
@@ -142,7 +185,7 @@ def prompt_model(text, image):
                 "content": content,
             }
         ],
-        "max_tokens": 300,
+        "max_tokens": 1000,
         "temperature": 0.2
     }
 
@@ -177,7 +220,7 @@ def prompt_model(text, image):
         return ""
 
 def parse_instructions_prompt(prompt):
-    reply = prompt_model(prompt_instruction_parser + prompt, None)
+    reply = prompt_model(prompt_instruction_parser.format(prompt), None)
 
     try:
         command_data = json.loads(reply.strip())
@@ -203,10 +246,39 @@ def parse_instructions_prompt(prompt):
 
     return None
 
+def detect_targets_in_image(instructions, image):
+    reply = prompt_model(prompt_object_detection.format(instructions["target"], instructions["desc"]), image)
+    try:
+        items = []
+        image_width, image_height = VLM_IMAGE_SIZE
+        for item in json.loads(reply.strip()):
+            box = item["box"]
+
+            true_x_min = int((box["x_min"] / 1000.0) * image_width)
+            true_y_min = int((box["y_min"] / 1000.0) * image_height)
+            true_x_max = int((box["x_max"] / 1000.0) * image_width)
+            true_y_max = int((box["y_max"] / 1000.0) * image_height)
+
+            items.append({
+                "desc": item["desc"],
+                "box":  {
+                    "x_min": true_x_min,
+                    "y_min": true_y_min,
+                    "x_max": true_x_max,
+                    "y_max": true_y_max,
+                },
+            })
+        return items
+
+    except ValueError as e:
+        # This catches JSON parsing errors if the LLM hallucinates bad formatting
+        rospy.logerr("Failed to parse JSON from LLM. Raw output was: %s", reply)
+        return None
+
 if __name__ == '__main__':
     rospy.init_node('pilot_interface', anonymous=True)
 
     pilot = Pilot()
-    pilot.execute_task("Go to the board!")
+    pilot.execute_task("Go to the bench next to a person!")
 
     rospy.spin()
